a5dbce07b8600452e020820d65d9592da4431501
takkar94 <ghimireaditya@gmail.com>
2024-06-05 11:34:14 +0545

    creating routes and controller for user

---
+++ backend/models/userModel.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please add a name']
+    },
+    email: {
+        type: String,
+        required: [true, 'Please add a password']
+    },
+    name: {
+        type: String,
+        required: [true, 'Please add a password']
+    }
+})
+
+module.exports = mongoose.model('User', userSchema);
--- backend/server.js
+++ backend/server.js
@@ -8,31 +8,10 @@ const connectDB = require('./config/db');
 const app = express();
 connectDB();
 
-// MongoDB Connection URL
-// const mongoURL = 'mongodb://127.0.0.1:27017';
-
-// // Create a new MongoClient
-// const client = new MongoClient(mongoURL);
-
-// // Connect to the MongoDB server
-// async function run() {
-//     try {
-//       // Connect the client to the server
-//       await client.connect();
-//       // Establish and verify connection
-//       await client.db("goals").command({ ping: 1 });
-//       console.log("Connected successfully to MongoDB server");
-//     } finally {
-//       // Ensures that the client will close when you finish/error
-//       await client.close();
-//     }
-// }
-  
-// run().catch(console.dir);
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/api/goals', require('./routes/goalRoutes'));
+app.use('/api/users', require('./routes/userRoutes'));
 app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
