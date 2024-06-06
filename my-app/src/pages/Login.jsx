c6580579f70ecef8a64a1ac34235345988700117
takkar94 <ghimireaditya@gmail.com>
2024-06-06 17:30:11 +0545

    solved the logout issue plus completed login and logout auth

--- my-app/src/pages/Login.jsx
+++ my-app/src/pages/Login.jsx
@@ -1,34 +1,65 @@
-import { useState, useEffect } from "react"
-import { FaSignInAlt } from "react-icons/fa";
+import { useState, useEffect } from 'react'
+import { FaSignInAlt } from 'react-icons/fa'
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { login, reset } from '../features/auth/authSlice'
+import Spinner from '../components/Spinner'
 
 function Login() {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  })
 
-    const [formData, setFormData] = useState({
-        email: '',
-        password: '',
+  const { email, password } = formData
 
-    })
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
 
-    const {email, password} = formData;
+  const { user, isLoading, isError, isSuccess, message } = useSelector((state) => state.auth)
 
-    const onChange = (e) => {
-        setFormData((prevState)=> ({
-            ...prevState,
-            [e.target.name] : e.target.value
-        }))
+  useEffect(() => {
+    if (isError) {
+      toast.error(message)
     }
 
-    const onSubmit = (e) => {
-        e.preventDefault();
+    if (isSuccess || user) {
+      navigate('/')
     }
+
+    dispatch(reset())
+  }, [user, isError, isSuccess, message, navigate, dispatch])
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }))
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    const userData = {
+      email,
+      password,
+    }
+
+    dispatch(login(userData))
+  }
+
+  if (isLoading) {
+    return <Spinner />
+  }
 
   return (
     <>
-      <section className="heading">
+      <section className='heading'>
         <h1>
-            <FaSignInAlt/> Sign In
+          <FaSignInAlt /> Login
         </h1>
-        <p>Please login to your account</p>
+        <p>Login and start setting goals</p>
       </section>
 
       <section className='form'>
@@ -55,6 +86,7 @@ function Login() {
               onChange={onChange}
             />
           </div>
+
           <div className='form-group'>
             <button type='submit' className='btn btn-block'>
               Submit
@@ -67,4 +99,3 @@ function Login() {
 }
 
 export default Login
-
