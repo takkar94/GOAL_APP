33951ae580f544ab55a46e960dd4326a4bf7b319
takkar94 <ghimireaditya@gmail.com>
2024-06-06 20:18:37 +0545

    integrated all the functionality to the front end

--- my-app/src/components/GoalForm.jsx
+++ my-app/src/components/GoalForm.jsx
@@ -1,12 +1,38 @@
 import { useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import {createGoal} from "../features/goals/goalsSlice"
 
 function GoalForm() {
+  
+  const [text, setText] = useState('');
+  const dispatch = useDispatch();
+
+
+  const onChange = (e) => {
+    setText(e.target.value);
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    dispatch(createGoal({text}));
+    setText('');
+  }
+
+
+
   return (
-    <div>
-      
-    </div>
+    <section className="form">
+        <form onSubmit={onSubmit}>
+            <div className="form-group">
+                <label htmlFor="text">Goal</label>
+                <input type='text' name='text' id='text' value={text} onChange={onChange}/>
+            </div>
+            <div className="form-group">
+                <button className="btn btn-block">Add Goal</button>
+            </div>
+        </form>
+    </section>
   )
 }
 
