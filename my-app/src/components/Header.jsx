a9fd5bff687b0d7de6f8ac1131ac7b2dc1bc6b77
takkar94 <ghimireaditya@gmail.com>
2024-06-06 12:26:26 +0545

    made a login and register layout

--- my-app/src/components/Header.jsx
+++ my-app/src/components/Header.jsx
@@ -1,8 +1,27 @@
+import {FaSignInAlt, FaSignOutAlt, FaUser} from 'react-icons/fa';
+import {Link} from 'react-router-dom'
+
 function Header() {
   return (
-    <div>
-      Header
-    </div>
+    <header className='header'>
+        <div className='logo'>
+            <Link to='/'>Goalsetter</Link>
+        </div>
+        <ul>
+            <li>
+            <Link to='/login'>
+                <FaSignInAlt/>Login
+            </Link>
+            </li>
+        </ul>
+        <ul>
+            <li>
+            <Link to='/register'>
+                <FaUser/>Register
+            </Link>
+            </li>
+        </ul>
+    </header>
   )
 }
 
